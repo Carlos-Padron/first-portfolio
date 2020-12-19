3d54017e8aea49555d9c3af7d36e94a1c69023cf
Carlos Padron <c_0509@hotmail.com>
2020-12-19 14:43:19 -0600

    about done, experience was stared

--- js/index.js
+++ js/index.js
@@ -1,15 +1,40 @@
 const navSlide = () => {
-  let isOpened = false;
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
 
   burger.addEventListener("click", () => {
     //  isOpened = false;
 
     nav.classList.toggle("nav-active");
-    burger.classList.toggle('toggle')
+    burger.classList.toggle("toggle");
   });
 };
 
+function changeTabs() {
+  let tabs = document
+    .querySelectorAll(".tab-header")[0]
+    .getElementsByTagName("div");
+
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener("click", function () {
+      document
+        .querySelectorAll(".tab-header")[0]
+        .getElementsByClassName("active")[0]
+        .classList.remove("active");
+
+      tabs[i].classList.add("active");
+
+      document
+        .querySelectorAll(".tab-content")[0]
+        .getElementsByClassName("active")[0]
+        .classList.remove("active");
+      document
+        .querySelectorAll(".tab-content")[0]
+        .getElementsByTagName("div")
+        [i].classList.add("active");
+    });
+  }
+}
+
 navSlide();
+changeTabs();
